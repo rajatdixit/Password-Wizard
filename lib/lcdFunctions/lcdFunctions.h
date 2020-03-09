abfd6c238a0bec9eea422c3b06dfda2c6e0fbcd2
rajatdixit007 <rajatdixit007@gmail.com>
2020-03-08 17:26:10 -0700

    Support for 2x16 LCD for Mega2560R3

---
+++ lib/lcdFunctions/lcdFunctions.h
@@ -0,0 +1,25 @@
+//This file defines all LCD related functions used in our project
+/*
+  The circuit for Arduino Mega2560 R3
+ * LCD VSS    pin to            GND - LOW
+ * LCD VDD    pin to digital pin 53 - HIGH
+ * LCD VO     pin to digital pin 51 - HIGH 
+ * LCD RS     pin to digital pin 49 - XXXX
+ * LCD RW     pin to digital pin 47 - LOW
+ * LCD E      pin to digital pin 45 - XXXX
+ * LCD D0     pin to digital pin 43 - XXXX
+ * LCD D1     pin to digital pin 41 - XXXX
+ * LCD D2     pin to digital pin 39 - XXXX
+ * LCD D3     pin to digital pin 37 - XXXX
+ * LCD D4     pin to digital pin 35 - XXXX
+ * LCD D5     pin to digital pin 33 - XXXX
+ * LCD D6     pin to digital pin 31 - XXXX
+ * LCD D7     pin to digital pin 29 - XXXX
+ * LCD A      pin to digital pin 27 - HIGH
+ * LCD K      pin to digital pin 25 - LOW
+*/
+
+///-----------------------Variable Definitions--------------------------///
+
+///-----------------------Functions Declarations------------------------///
+void initializeLCD();
