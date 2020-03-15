7b4bdf2e68d19058509b80790f10a8f326647e64
rajatdixit007 <rajatdixit007@gmail.com>
2020-03-15 12:35:32 -0700

    Added lcdFunctions code with a test print

--- lib/lcdFunctions/lcdFunctions.cpp
+++ lib/lcdFunctions/lcdFunctions.cpp
@@ -1,20 +1,30 @@
-#include <LiquidCrystal.h>
+#include "LiquidCrystal.h"
+#include "Arduino.h"
+#include "lcdFunctions.h"
+#include "Wire.h"
 
-// initialize the library with the numbers of the interface pins
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+LiquidCrystal lcd(RS, E, D0, D1, D2, D3, D4, D5, D6, D7);
 
-void setup() {
-  // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
+void initializeLCD()
+{
+  //pinMode
+  pinMode(VDD, OUTPUT);
+  pinMode(VO, OUTPUT);
+  pinMode(RW, OUTPUT);
+  pinMode(A, OUTPUT);
+  pinMode(K,OUTPUT);
+  
+  //digitalWrite
+  digitalWrite(VDD, HIGH);
+  digitalWrite(VO, LOW);
+  digitalWrite(RW, LOW);
+  digitalWrite(A, HIGH);
+  digitalWrite(K, LOW);
+
+  lcd.begin(16,2);
 }
-
-void loop() {
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
+void printTestMessage() 
+{
+  lcd.print("R");
 }
 
